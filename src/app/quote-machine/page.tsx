7c45da47430cefccf22ef8f6c7673a6051334a96
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-09-25 20:16:09 +0300

    fix: lint + fix type errors

--- src/app/quote-machine/page.tsx
+++ src/app/quote-machine/page.tsx
@@ -1,16 +1,20 @@
-import Quote from './Quote'
-import { APIResponse } from './types'
-type Props = {}
+"use server";
 
-export default async function QuoteMachine({ }: Props) {
-	
-	const quotes: APIResponse = await (await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json", {cache: 'force-cache'})).json()
-	
+import Quote from "./Quote";
+import type { APIResponse } from "./types";
+
+export default async function QuoteMachine() {
+	const quotes = (await (
+		await fetch(
+			"https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+			{ cache: "force-cache", next: { revalidate: false } },
+		)
+	).json()) as APIResponse;
 
 	return (
-		<div className="grid h-screen min-w-8 max-w-1/3 p-8 place-items-center">
-	  {/*~~ is a faster version of Math.floor*/}
+		<div className="min-w-8 max-w-1/3 grid h-screen place-items-center p-8">
+			{/*~~ is a faster version of Math.floor*/}
 			<Quote {...quotes.quotes[~~(Math.random() * quotes.quotes.length)]}></Quote>
-			</div>
-  )
+		</div>
+	);
 }
