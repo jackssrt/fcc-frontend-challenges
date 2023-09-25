534cc1be17830a7ee952dfa6352d050d544fcfde
jackssrt <33695699+jackssrt@users.noreply.github.com>
2023-09-25 20:39:40 +0300

    fix: enable server actions + fix quote caching

--- src/app/quote-machine/page.tsx
+++ src/app/quote-machine/page.tsx
@@ -7,7 +7,7 @@ export default async function QuoteMachine() {
 	const quotes = (await (
 		await fetch(
 			"https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
-			{ cache: "force-cache", next: { revalidate: false } },
+			{ next: { revalidate: false } },
 		)
 	).json()) as APIResponse;
 
